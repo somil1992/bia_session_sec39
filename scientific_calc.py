9d0a9919639f64c0b99cf5dff7f4917e957cdc88
somil1992 <33191690+somil1992@users.noreply.github.com>
2024-04-21 16:24:38 +0530

    Add files via upload

---
+++ scientific_calc.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+def scientific_calculator(operation,x,y):
+    if operation == 'add':
+        result = x + y
+    elif operation == 'difference':
+        result = x-y
+    elif operation == 'multiply':
+        result = x*y
+    return result
+
+
+# In[ ]:
+
+
+
+
